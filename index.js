7ffee14d9390901c7562eac9816faf16ab7be435
Tyler <tyler.meyer.a@gmail.com>
2024-10-02 11:17:52 -0500

    Created some HTML structure

--- index.js
+++ index.js
@@ -3,6 +3,42 @@ $(document).ready(() => {
   const $body = $('body');
   $body.html(''); // Clears the body
 
+  // Create #all-contents div and append to $body
+  const $allContentsDiv = $('<div>')
+    .attr('id', 'all-contents')
+    .attr('class', 'all-contents');
+  $body.append($allContentsDiv);
+
+  // Create <nav> and append to $allContentsDiv
+  const $nav = $('<nav>');
+  $allContentsDiv.append($nav);
+
+  // Create <main> and append to $allContentsDiv
+  const $main = $('<main>');
+  $allContentsDiv.append($main);
+
+  // Create #sidebar div and append to $main
+  const $sidebarDiv = $('<div>')
+    .attr('id', 'sidebar')
+    .attr('class', 'sidebar');
+  $main.append($sidebarDiv);
+
+  // Create #content div and append to $main
+  const $contentDiv = $('<div>')
+    .attr('id', 'content')
+    .attr('class', 'content');
+  $main.append($contentDiv);
+
+  // Create #top-input-tweet div and append to $contentDiv
+  const $topInputTweetDiv = $('<div>')
+    .attr('id', 'top-input-tweet');
+  $contentDiv.append($topInputTweetDiv);
+
+  // Create #tweet-feed div and append to $contentDiv
+  const $tweetFeedDiv = $('<div>')
+    .attr('id', 'tweet-feed');
+  $contentDiv.append($tweetFeedDiv);
+
 function createTweets() {
   const $tweets = streams.home.map((tweet) => {
     const $tweet = $('<div></div>');
@@ -12,7 +48,9 @@ function createTweets() {
 
     return $tweet;
   });
-  $body.append($tweets);
+  $tweetFeedDiv.append($tweets);
 }
+
+createTweets();
 
 });
