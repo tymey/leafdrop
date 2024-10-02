53b7ba59dd3ad868757bcfb700ec1d81fdfaba3c
Tyler <tyler.meyer.a@gmail.com>
2024-10-02 10:39:52 -0500

    createTweets() function added to generate tweets on function call

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ $(document).ready(() => {
   const $body = $('body');
   $body.html(''); // Clears the body
 
+function createTweets() {
   const $tweets = streams.home.map((tweet) => {
     const $tweet = $('<div></div>');
     const text = `@${tweet.user}: ${tweet.message}`;
@@ -12,5 +13,6 @@ $(document).ready(() => {
     return $tweet;
   });
   $body.append($tweets);
+}
 
 });
